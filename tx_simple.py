ce902ded9a5951fcd8e6e3b2c0fce2429eccd839
shioneto <seto@umass.edu>
2026-02-25 14:27:58 -0500

    First commit

---
+++ tx_simple.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+
+# --- User settings ---
+dat_path = "Tx/waveforms/hi_low_I_5Mhzfs_3sec.dat"
+center_freq = 1e6 # Hz
+sample_rate = 5_000_000 # Must match waveform created
+tx_gain = 10                                # dB
+amp_enable = True                       # use RF amplifier
+num_samples = sample_rate * 3
+
+
+
+# --- Build hackrf_transfer comman
+cmd = [
+    "hackrf_transfer",
+    "-t", dat_path,                      # transmit file
+    "-f", str(center_freq),              # frequency
+    "-s", str(sample_rate),              # sample rate
+    "-x", str(tx_gain),                  # TX VGA gain
+    "-n", str(num_samples)
+]
+
+
+
+if amp_enable:
+    cmd += ["-a", "1"]                   # enable amp
+
+# --- Run command ---
+print("Running:", " ".join(cmd))
+subprocess.run(cmd, check=True)
+print("Transmission complete.")
+
